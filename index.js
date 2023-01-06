ac2378e2ddfc3498556a89d7e84edd899488cb23
EliteWebmaster <44509681+elitewebmaster@users.noreply.github.com>
2023-01-06 15:11:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ unpackArchive();
 
 module.exports.handler = (event, context, cb) => {
   const filepath = event.filepath,
-   s3 = new AWS.S3(),
-   options = {
-    Bucket: process.env.S3_BUCKET_NAME,
-    Key: filepath,
-  };
+         s3 = new AWS.S3(),
+         options = {
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: filepath,
+        };
 
   s3.getObject(options, function(error, data) {
     if(error){
       console.log(error); 
       cb(error);
     } else {
-      attachment = data.Body;
-      let arrPath = filepath.split("/");
-      let filename = arrPath[arrPath.length - 1];
-      let randomFilename = (Math.random().toString(32).slice(2)) + (filename.slice(filename.lastIndexOf("."), filename.length));
-      const fileextension = (event.fileextension) ? event.fileextension.toLowerCase() : "pdf";
+      const attachment = data.Body,
+            arrPath = filepath.split("/"),
+            filename = arrPath[arrPath.length - 1],
+            randomFilename = (Math.random().toString(32).slice(2)) + (filename.slice(filename.lastIndexOf("."), filename.length)),
+            fileextension = (event.fileextension) ? event.fileextension.toLowerCase() : "pdf";
       
       return convertFileToPDF(attachment, filename, arrPath, fileextension, randomFilename)
       .then(fileURL => {
